69c8a966803dec550492176c0ab15182797e9cec
Palnetguy <tusingwiremartinrhinetreviz@gmail.com>
2025-06-14 14:03:45 +0300

    commit #2

--- task_3.c
+++ task_3.c
@@ -9,6 +9,14 @@ int main() {
     float average_sales = total_sales / days;
     printf("Average sales per Day: %.2f\n\n", average_sales);
 
+    float sales_volume, commision_rate;
+    printf("Enter sales volume: ");
+    scanf("%f", &sales_volume);
+    printf("Enter commision rate: ");
+    scanf("%f", &commision_rate);
+    float commision = sales_volume * (commision_rate / 100);
+    printf("Commision: %.2f\n\n", commision);
+
     return 0;
 
 }
